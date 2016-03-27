048993ceaa08ab91a82dfea89dbf5db27a8c5379
jtr4fh <jtr4fh@virginia.edu>
2016-03-26 23:12:23 -0400

    AngularJS
    Adds AngularJS to app.

--- drink_maker/drink_maker/static/drink_maker/js/app.js
+++ drink_maker/drink_maker/static/drink_maker/js/app.js
@@ -1,84 +1,76 @@
 
-var recipes = [],
-	liquids = [],
-	valves = [],
-	views = ["drink-maker-container","recipe-editor-container","liquid-editor-container","valve-config-container"];
 
-var loadDrinkMakerHtml = function(recipes) {
-	var html = '<div class="row">';
-	recipes.forEach(function(recipe) {
-		html += '<div class="col-lg-6 col-md-6 col-sm-12">';
-		html += '<div class="panel panel-primary dm-recipe-container">'
-		html += '<div class="panel-heading"><h2 class="panel-title">' + recipe.name + '</h2></div>';
-		html += '<div class="panel-body">';
-		recipe.components.forEach(function(c) {
-			html += '<p><span class="bold">' + c.name + ':</span> ' + c.volume + 'mL</p>';
-		});
-		html += '</div></div></div>';
-	});
-	html += '</div>';
-	$("#dmRecipesContainer").html(html);
-};
+// var loadRecipeHtml = function(recipes) {
+// 	var html = "";
+// 	recipes.forEach(function(recipe) {
+// 		html += '<div class="re-recipe-container>"';
 
-var swapViews = function(view) {
-	views.forEach(function(v) {
-		if(view !== v) { $('.'+v).hide(); }
-	});
-	$('.'+view).show();
-}
+// 		html += '</div>';
+// 	});
+// }
 
-var loadRecipeHtml = function(recipes) {
-	var html = "";
-	recipes.forEach(function(recipe) {
-		html += '<div class="re-recipe-container>"';
+// var getRecipes = function() {
+// 	$.ajax({
+// 		url: "/recipes/",
+// 	    type: "GET",
+// 	    success: function(json) {
+// 	      	recipes = json["recipes"];
 
-		html += '</div>';
-	});
-}
+// 	    	loadDrinkMakerHtml(recipes);
+// 	    	loadRecipeHtml(recipes);
+// 	    }
+// 	});
+// };
 
-var getRecipes = function() {
-	$.ajax({
-		url: "/recipes/",
-	    type: "GET",
-	    success: function(json) {
-	      	recipes = json["recipes"];
+// var getValves = function() {
+// 	$.ajax({
+// 		url: "/valves/",
+// 	    type: "GET",
+// 	    success: function(json) {
+// 	      	valves = json["valves"];
+// 	    }
+// 	});
+// };
 
-	    	loadDrinkMakerHtml(recipes);
-	    	loadRecipeHtml(recipes);
-	    }
-	});
-};
+// var getLiquids = function() {
+// 	$.ajax({
+// 		url: "/liquids/",
+// 	    type: "GET",
+// 	    success: function(json) {
+// 	      	liquids = json["liquids"];
+// 	    }
+// 	});
+// };
 
-var getValves = function() {
-	$.ajax({
-		url: "/valves/",
-	    type: "GET",
-	    success: function(json) {
-	      	valves = json["valves"];
-	    }
-	});
-};
+// var pollDrinkRequest = function() {
+// 	$.ajax({
+// 		url: "/drinks/",
+// 	    type: "GET",
+// 	    success: function(json) {
+// 	      	if(json.length > 0) {
+// 	      		console.log("There is a request being fulfilled");
+// 	      	}
+// 	    }
+// 	});
+// }
 
-var getLiquids = function() {
-	$.ajax({
-		url: "/liquids/",
-	    type: "GET",
-	    success: function(json) {
-	      	liquids = json["liquids"];
-	    }
-	});
-};
+// getRecipes();
 
-var pollDrinkRequest = function() {
-	$.ajax({
-		url: "/drinks/",
-	    type: "GET",
-	    success: function(json) {
-	      	if(json.length > 0) {
-	      		console.log("There is a request being fulfilled");
-	      	}
-	    }
-	});
-}
+angular.module('Barkeep', [
+	'ngResource'
+])
 
-getRecipes();
+.controller('MainCtrl', ['$scope', 'RestApi', function ($scope, RestApi) {
+	$scope.recipes = RestApi.getRecipes()
+	$scope.view = {value: 'home'};
+}])
+
+.factory('RestApi', ['$resource', function ($resource) {
+	var recipeApi = $resource('/recipes/:recipeId', {recipeId: 'recipe_pk'});
+
+	return {
+		getRecipes: function () {
+			return recipeApi.query();
+		}
+	};
+}]);
